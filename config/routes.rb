cc7148fdf000317c35d6a350c0ba8fdc662ed1d0
Konstantin82off <k.udodov@internet.ru>
2026-03-18 17:12:49 +0300

    step 2: Add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Корневой маршрут - главная страница
   root 'home#index'
 
+  # Auth routes
+  post 'auth/:provider', to: 'auth#request', as: :auth_request
+  get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
