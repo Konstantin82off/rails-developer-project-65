a240b0d08fef37ff130c0e4abdb0762280303f5c
Konstantin82off <k.udodov@internet.ru>
2026-03-19 08:44:30 +0300

    step 3: Add Category and Bulletin models with web controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_18_140558) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_19_054235) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
