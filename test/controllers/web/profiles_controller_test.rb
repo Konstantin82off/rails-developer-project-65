690af8d1c7f4f08ecccab499496ac8e2a4869a8a
Konstantin82off <k.udodov@internet.ru>
2026-03-25 20:37:51 +0300

    step-7: add kaminari bootstrap5 templates and profiles controller test

---
+++ test/controllers/web/profiles_controller_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Web
+  class ProfilesControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      @user = users(:one)
+    end
+
+    test 'should get show when signed in' do
+      sign_in(@user)
+      get profile_path
+      assert_response :success
+    end
+
+    test 'should not get show when not signed in' do
+      get profile_path
+      assert_redirected_to '/auth/github'
+    end
+  end
+end
