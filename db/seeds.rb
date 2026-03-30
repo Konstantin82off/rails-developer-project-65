6f6b428b72e724d5a424dae3bbf3aa3529c6f3e8
Konstantin82off <k.udodov@internet.ru>
2026-03-30 21:38:38 +0300

    step-7: add admin email k.udodov@internet.ru to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Создаем администратора
-User.find_or_create_by!(email: 'k.udodov@bk.ru') do |user|
+User.find_or_create_by!(email: 'k.udodov@internet.ru') do |user|
   user.name = 'Admin'
   user.admin = true
 end
