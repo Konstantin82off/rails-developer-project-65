94b2846f85b1aac45471dba889f1d96f39336274
Konstantin82off <k.udodov@internet.ru>
2026-03-31 15:45:50 +0300

    step-7: integrate Rollbar error tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,28 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  before_action :set_rollbar_context, if: -> { defined?(Rollbar) }
+
   private
 
   def user_not_authorized
     redirect_to root_path, alert: t('admin.access_denied')
+  end
+
+  def set_rollbar_context
+    Rollbar.scope!(
+      person: {
+        id: current_user&.id,
+        username: current_user&.name,
+        email: current_user&.email
+      },
+      custom: {
+        request_id: request.uuid,
+        user_agent: request.user_agent,
+        ip_address: request.remote_ip,
+        controller: params[:controller],
+        action: params[:action]
+      }
+    )
   end
 end
