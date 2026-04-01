96cd9c061e51ad2f191de190ad648a217e51dcb7
Konstantin82off <k.udodov@internet.ru>
2026-04-01 14:47:45 +0300

    step-7: add help cursor to all required field indicators

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// Инициализация Bootstrap tooltips
+document.addEventListener('turbo:load', () => {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})
