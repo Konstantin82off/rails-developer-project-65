3ba82722cb67cdbf22d3e9186e805f8228738952
Konstantin82off <k.udodov@internet.ru>
2026-04-08 16:37:41 +0300

    test: add model tests for User, Category, Bulletin
    - User: presence of email/name, uniqueness of email, admin? predicate
    - Category: presence of name
    - Bulletin: presence of title/description/user/category, length validations, AASM state transitions

--- test/models/bulletin_test.rb
+++ test/models/bulletin_test.rb
@@ -3,7 +3,81 @@
 require 'test_helper'
 
 class BulletinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: 'test@example.com', name: 'Test User')
+    @category = Category.create!(name: 'Test Category')
+    @bulletin = Bulletin.new(
+      title: 'Test Title',
+      description: 'Test Description ' * 10,
+      user: @user,
+      category: @category
+    )
+  end
+
+  test 'should be valid' do
+    assert @bulletin.valid?
+  end
+
+  test 'title should be present' do
+    @bulletin.title = '   '
+    assert_not @bulletin.valid?
+  end
+
+  test 'title should not be too long' do
+    @bulletin.title = 'a' * 51
+    assert_not @bulletin.valid?
+  end
+
+  test 'description should be present' do
+    @bulletin.description = '   '
+    assert_not @bulletin.valid?
+  end
+
+  test 'description should not be too long' do
+    @bulletin.description = 'a' * 1001
+    assert_not @bulletin.valid?
+  end
+
+  test 'should have user' do
+    @bulletin.user = nil
+    assert_not @bulletin.valid?
+  end
+
+  test 'should have category' do
+    @bulletin.category = nil
+    assert_not @bulletin.valid?
+  end
+
+  test 'should have initial state draft' do
+    assert_equal 'draft', @bulletin.state
+  end
+
+  test 'can transition from draft to under_moderation' do
+    assert @bulletin.may_to_moderate?
+    @bulletin.to_moderate
+    assert_equal 'under_moderation', @bulletin.state
+  end
+
+  test 'can transition from under_moderation to published' do
+    @bulletin.state = 'under_moderation'
+    assert @bulletin.may_publish?
+    @bulletin.publish
+    assert_equal 'published', @bulletin.state
+  end
+
+  test 'can transition from under_moderation to rejected' do
+    @bulletin.state = 'under_moderation'
+    assert @bulletin.may_reject?
+    @bulletin.reject
+    assert_equal 'rejected', @bulletin.state
+  end
+
+  test 'can archive from any non-archived state' do
+    %w[draft under_moderation published rejected].each do |state|
+      @bulletin.state = state
+      assert @bulletin.may_archive?
+      @bulletin.archive
+      assert_equal 'archived', @bulletin.state
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'test@example.com', name: 'Test User')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '   '
+    assert_not @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '   '
+    assert_not @user.valid?
+  end
+
+  test 'email should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'admin? returns false by default' do
+    assert_not @user.admin?
+  end
+
+  test 'admin? returns true when admin is true' do
+    @user.admin = true
+    assert @user.admin?
+  end
 end
