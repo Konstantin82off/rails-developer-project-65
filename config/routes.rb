804d3adf9fdc2caac325c0b652df593caa414f2c
Konstantin82off <k.udodov@internet.ru>
2026-04-09 12:43:48 +0300

    refactor: rename IndexController to HomeController for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin, module: 'web/admin', as: :admin do
-    root to: 'index#index'
+    root to: 'home#index'
     resources :categories
     resources :bulletins, only: :index do
       member do
