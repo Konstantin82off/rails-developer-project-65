5ecc442b745e34b32f386fac6c08786fe15c196c
Konstantin82off <k.udodov@internet.ru>
2026-04-09 19:20:46 +0300

    refactor: replace before_action with explicit authentication check in ProfilesController

--- app/controllers/web/profiles_controller.rb
+++ app/controllers/web/profiles_controller.rb
@@ -2,9 +2,12 @@
 
 module Web
   class ProfilesController < ApplicationController
-    before_action :authenticate_user!
-
     def show
+      unless signed_in?
+        redirect_to '/auth/github'
+        return
+      end
+
       @q = current_user.bulletins.ransack(params[:q])
       @bulletins = @q.result.ordered.page(params[:page]).per(10)
     end
