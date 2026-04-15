48c506950edf2f9bbbad6e5eb947c41b23ddccd3
Konstantin82off <k.udodov@internet.ru>
2026-04-15 17:04:03 +0300

    fix: working omniauth config with provider_ignores_state only for development

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -2,19 +2,12 @@
 
 module Web
   class AuthController < Web::ApplicationController
-    skip_before_action :verify_authenticity_token, only: :callback
-
     def callback
       auth_hash = request.env['omniauth.auth']
       return redirect_to(root_path, alert: t('auth.failure')) if auth_hash.nil?
 
       user = find_or_create_user(auth_hash)
       session[:user_id] = user.id
-
-      Rails.logger.info '=== USER LOGIN ==='
-      Rails.logger.info "User email: #{user.email}"
-      Rails.logger.info "User admin: #{user.admin?}"
-      Rails.logger.info '================='
 
       redirect_to root_path, notice: t('auth.success')
     end
@@ -33,7 +26,7 @@ module Web
       return user unless user.new_record?
 
       user.name = auth_hash[:info][:name]
-      user.save!
+      user.save
       user
     end
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-OmniAuth.config.logger = Rails.logger
-OmniAuth.config.full_host = lambda do |_env|
-  if Rails.env.development?
-    'http://localhost:3000'
-  else
-    'https://rails-developer-project-65-kwud.onrender.com'
-  end
-end
-OmniAuth.config.allowed_request_methods = %i[get post]
-OmniAuth.config.silence_get_warning = true
+OmniAuth.config.full_host = if Rails.env.development?
+                              'http://localhost:3000'
+                            else
+                              'https://rails-developer-project-65-kwud.onrender.com'
+                            end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV.fetch('GITHUB_CLIENT_ID', ''), ENV.fetch('GITHUB_CLIENT_SECRET', ''),
-           scope: 'user',
-           provider_ignores_state: true
+  provider :developer unless Rails.env.production?
+
+  github_options = { scope: 'user' }
+  github_options[:provider_ignores_state] = true if Rails.env.development?
+
+  provider :github, ENV.fetch('GITHUB_CLIENT_ID', ''), ENV.fetch('GITHUB_CLIENT_SECRET', ''), github_options
 end
